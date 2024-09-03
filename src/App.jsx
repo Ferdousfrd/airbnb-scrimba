bb20ee59e5441b54d410e10629bee87b05c2cf8e
Ferdousfrd <ferdous.frd81@gmail.com>
2024-09-03 17:30:26 +0300

    done but had iisue with image prop

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,29 @@
 import React  from "react";
 import Header from "./components/Header";
 import Card from "./components/Card";
+import data from "../data.js"
 
 import "./styles/app.css"
 
 export default function App(){
+    const cards = data.map(item => {
+        return <Card 
+                    key = {item.id}
+
+                    rating = {item.stats.rating}
+                    reviewCount = {item.stats.reviewCount}
+                    location = {item.location}
+                    title = {item.title}
+                    price = {item.price}
+                />
+    })
+
     return(
         <>
             <Header />
             <div className="cards-section">
-                <Card />
-                <Card />
-                <Card />
+                {cards}
+
             </div>
         </>
     )
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,19 +2,21 @@ import React from "react";
 import cardImg from "../assets/katie-zaferes.png"
 import starImg from "../assets/star.png"
 
-export default function Card(){
+
+
+export default function Card(props){
     return (
         <div className="card">
-            <img className="card-img" src={cardImg} />
+            
             <div className="card-stats">
                 <img className="card-star" src={starImg} />
-                <p>5.0</p>
-                <p className="grey">(6)</p>
+                <p>{props.rating}</p>
+                <p className="grey">({props.reviewCount})</p>
                 <p className="grey" > . </p>
-                <p className="grey">USA</p>
+                <p className="grey">{props.location}</p>
             </div>
-            <p className="card-title">Life lessons with katie</p>
-            <h5 className="bold">From $136 <span className="grey">/ person </span></h5>
+            <p className="card-title">{props.title}</p>
+            <h5 className="bold">From ${props.price} <span className="grey">/ person </span></h5>
 
         </div>
     )
